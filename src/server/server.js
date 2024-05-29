4e3fb1846ad3ef7ab6496ce31d46e9347bc7f6ae
ridwanr00 <rasyid.r00@gmail.com>
2024-05-29 16:57:18 +0700

    Push code to remote repository

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,46 @@
 const Hapi = require('@hapi/hapi')
+const routes = require('./routes')
+const InputError = require('../exceptions/InputError')
+require('dotenv').config()
+const loadModel = require('../services/loadModel')
 
 const init = async () => {
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost',
+        host: '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*']
             }
         }
+    })
+
+    const model = await loadModel()
+    server.app.model = model
+
+    server.route(routes)
+
+    server.ext('onPreResponse', function (request, h) {
+        const response = request.response
+        if (response instanceof InputError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: 'Terjadi kesalahan dalam melakukan prediksi'
+            })
+            newResponse.code(response.statusCode)
+            return newResponse
+        }
+
+        if (response.isBoom) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: response.message
+            })
+            newResponse.code(413)
+            return newResponse
+        }
+
+        return h.continue
     })
 
     await server.start()
