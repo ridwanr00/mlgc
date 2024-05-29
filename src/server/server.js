745019bb24933542cc04ca31fc88eb84707e27a1
ridwanr00 <rasyid.r00@gmail.com>
2024-05-29 18:56:11 +0700

    Push code to remote repository

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@ const loadModel = require('../services/loadModel')
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         host: '0.0.0.0',
         routes: {
             cors: {
